3e460025467ba2c46e0709f1d99feeab16860931
haven <haven@woowahan.com>
2024-05-25 06:12:32 +0900

    qa_item: rearrange css

--- src/components/QaItem/QaItem.tsx
+++ src/components/QaItem/QaItem.tsx
@@ -1,6 +1,8 @@
 import { TQaItem } from '@/store/qa/qa';
 import { Button } from '../ui/button';
 import useQaItemController from './QaItem.controller';
+import React, { useState, useEffect, useRef, RefObject } from 'react';
+import './QaItem.css';
 
 export interface IProps extends TQaItem {}
 
@@ -9,16 +11,45 @@ const QaItem = (props: IProps) => {
   const { type, question, answer1, answer2 } = props;
   const isMusic = type == 'MUSIC';
 
+  const [currentAudio, setCurrentAudio] = useState<HTMLAudioElement | null>(null);
+  const musicRef1 = useRef<HTMLAudioElement>(null);
+  const musicRef2 = useRef<HTMLAudioElement>(null);
+
+  const handlePlay = (musicRef: RefObject<HTMLAudioElement>) => {
+    if (currentAudio && currentAudio !== musicRef.current) {
+      currentAudio.pause();
+    }
+
+    setCurrentAudio(musicRef.current);
+  };
+
   return (
-    <div>
-      {type}
-      {question}
+    <div className="grid-container">
+      <div className="grid-item question-title">{question}</div>
       <div className="">
-        <Button variant="primary" size="full" onClick={qaItem.nextQa}>
-          {isMusic ? <audio controls src={'public/music/' + answer1.answer + '.mp3'}></audio> : answer1.answer}
+        <Button className="answer-card" size="full" onClick={qaItem.nextQa}>
+          {isMusic ? (
+            <audio
+              ref={musicRef1}
+              controls
+              src={'public/music/' + answer1.answer + '.mp3'}
+              onPlay={() => handlePlay(musicRef1)}
+            ></audio>
+          ) : (
+            answer1.answer
+          )}
         </Button>
-        <Button variant="primary" size="full" onClick={qaItem.nextQa}>
-          {isMusic ? <audio controls src={'public/music/' + answer2.answer + '.mp3'}></audio> : answer2.answer}
+        <Button className="answer-card" size="full" onClick={qaItem.nextQa}>
+          {isMusic ? (
+            <audio
+              ref={musicRef2}
+              controls
+              src={'public/music/' + answer2.answer + '.mp3'}
+              onPlay={() => handlePlay(musicRef2)}
+            ></audio>
+          ) : (
+            answer2.answer
+          )}
         </Button>
       </div>
     </div>
