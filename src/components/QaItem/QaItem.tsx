ad4a9883d27c537e237afa2d9302642a704a6d0f
haven <haven@woowahan.com>
2024-05-25 05:00:09 +0900

    qa_item: add music play feature

--- src/components/QaItem/QaItem.tsx
+++ src/components/QaItem/QaItem.tsx
@@ -7,6 +7,7 @@ export interface IProps extends TQaItem {}
 const QaItem = (props: IProps) => {
   const qaItem = useQaItemController();
   const { type, question, answer1, answer2 } = props;
+  const isMusic = type == 'MUSIC';
 
   return (
     <div>
@@ -14,10 +15,10 @@ const QaItem = (props: IProps) => {
       {question}
       <div className="">
         <Button variant="primary" size="full" onClick={qaItem.nextQa}>
-          {answer1.answer}
+          {isMusic ? <audio controls src={'public/music/' + answer1.answer + '.mp3'}></audio> : answer1.answer}
         </Button>
         <Button variant="primary" size="full" onClick={qaItem.nextQa}>
-          {answer2.answer}
+          {isMusic ? <audio controls src={'public/music/' + answer2.answer + '.mp3'}></audio> : answer2.answer}
         </Button>
       </div>
     </div>
