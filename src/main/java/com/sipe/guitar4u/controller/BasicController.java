08fe7ad87d8cc470d9d10f2109cb6f99a4c40181
유지훈 <yjh@qoo10.com>
2024-05-25 04:10:01 +0900

    계산 로직

--- src/main/java/com/sipe/guitar4u/controller/BasicController.java
+++ src/main/java/com/sipe/guitar4u/controller/BasicController.java
@@ -1,8 +1,12 @@
 package com.sipe.guitar4u.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -64,25 +68,70 @@ public class BasicController {
     @PostMapping("result")
     public ResponseEntity<Result> getResult(@RequestBody ChoiceResult choiceResult) {
         List<String> resultStrings = choiceResult.getChoiceResult();
+        Map<String, Double> resultMap = new HashMap<>();
+        resultMap.put("A", 0.0);
+        resultMap.put("B", 0.0);
+        resultMap.put("C", 0.0);
+        resultMap.put("N", 0.0);
+        resultMap.put("S", 0.0);
+        resultMap.put("F", 0.0);
+        resultMap.put("T", 0.0);
         for (String resultString : resultStrings) {
             switch (resultString) {
                 case "A":
+                    resultMap.put("A", resultMap.get("A")+1) ;
                     break;
                 case "B":
+                    resultMap.put("B", resultMap.get("B")+1) ;
                     break;
                 case "C":
+                    resultMap.put("C", resultMap.get("C")+1) ;
                     break;
                 case "N":
+                    resultMap.put("N", resultMap.get("N")+0.3) ;
                     break;
                 case "S":
+                    resultMap.put("S", resultMap.get("S")+0.3) ;
                     break;
                 case "T":
+                    resultMap.put("T", resultMap.get("T")+0.3) ;
                     break;
                 case "F":
+                    resultMap.put("F", resultMap.get("F")+0.3) ;
                     break;
             }
         }
-        Result result = resultRepository.findByCode("A");
+
+
+        String selectNS = resultMap.get("N") > resultMap.get("S") ? "N" : "S";
+        String selectTF = resultMap.get("T") > resultMap.get("F") ? "T" : "F";
+        String mbti = selectNS + selectTF;
+        Double mbtiScore = 0.0;
+        switch (mbti) {
+            case "ST":
+                mbtiScore = resultMap.get("S") + resultMap.get("T"); //1.2  + 1 2.2
+                resultMap.put("A", resultMap.get("A") + mbtiScore);
+                break;
+            case "SF":
+                mbtiScore = resultMap.get("S") + resultMap.get("F");
+                resultMap.put("A", resultMap.get("A") + mbtiScore);
+                break;
+            case "NT":
+                mbtiScore = resultMap.get("N") + resultMap.get("T");
+                resultMap.put("B", resultMap.get("B") + mbtiScore);
+                break;
+            case "NF":
+                mbtiScore = resultMap.get("N") + resultMap.get("F");
+                resultMap.put("C", resultMap.get("C") + mbtiScore);
+                break;
+        }
+
+        Optional<Entry<String, Double>> recommendGuitar = resultMap.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue());
+
+
+        Result result = resultRepository.findByCode(recommendGuitar.get().getKey());
         return ResponseEntity.ok().body(result);
     }
 }
---
+++ src/main/java/com/sipe/guitar4u/service/GuitarService.java
@@ -0,0 +1,9 @@
+package com.sipe.guitar4u.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GuitarService {
+
+
+}
